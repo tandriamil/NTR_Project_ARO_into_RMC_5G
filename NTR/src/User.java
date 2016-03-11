c2a2134ae178c16a5ba39eef8581ccdc1a129962
Quentin Guillou <15009070@e003m06.istic.univ-rennes1.fr>
2016-03-11 12:08:08 +0100

    Implements RoundRobin

--- NTR/src/User.java
+++ NTR/src/User.java
@@ -2,20 +2,39 @@ import java.util.PriorityQueue;
 
 
 public class User {
+	private int id;
 	private boolean near;
 	private AccessPoint accessPoint;
 	private PriorityQueue<Packet> buffer;
+	private int debitMoy;
+	private int debitCurrent;
 	
-	public User(AccessPoint accessPoint, boolean near) {
+	public User(int id, AccessPoint accessPoint, boolean near) {
+		this.id = id;
 		this.accessPoint = accessPoint;
 		this.near = near;
 		buffer = new PriorityQueue<Packet>();
+		if(near) {
+			debitMoy = 6;
+		}
+		else {
+			debitMoy = 3;
+		}
 	}
 	
 	public void createPacket() {
 		if((int)(Math.random() * 1000)%2 == 0){
 			Packet packet = new Packet(this, accessPoint.getTime());
 			buffer.add(packet);
+		}
+	}
+
+	public void checkPacket() {
+		Packet packet = this.getCurrentPacket();
+		int bitsLeft = packet.getNbBitsLeft() - debitCurrent;
+		if(bitsLeft <= 0){
+			bitsLeft = 0;
+			this.packetTerminated();
 		}
 	}
 	
@@ -26,5 +45,13 @@ public class User {
 	public void packetTerminated() {
 		getCurrentPacket().setEndSend(accessPoint.getTime());
 		buffer.remove();
+	}
+
+	public void calculateDebit() {
+		debitCurrent = 2*(int)(Math.random()*((debitMoy*2)/2+1));
+	}
+
+	public int getSNR() {
+		return 0;
 	}
 }
