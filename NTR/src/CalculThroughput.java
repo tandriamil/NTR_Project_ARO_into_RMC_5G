0ee3aa5b2452f947de60f333b686714d1f25a3df
Ihsine Azzeddine <azd.ihsine@gmail.com>
2016-03-15 11:49:31 +0100

    fix(calcul): Fixed the calcul class

--- NTR/src/CalculThroughput.java
+++ NTR/src/CalculThroughput.java
@@ -1,12 +1,15 @@
 
+import java.util.List;
+import java.util.Iterator;
 public class CalculThroughput implements Calculation{
 
 	private World world; 
 	private int time;
-	private int nb_bytes;
+    private int debitMax;
 
 	public CalculThroughput(World world){
 		this.world = world;
+        this.debitMax = 0;
 		this.time = this.world.getTime();
 	}
 
@@ -14,28 +17,31 @@ public class CalculThroughput implements Calculation{
 		return this.world;
 	}
 
+    public int getDebitMax(){
+        return this.debitMax;
+    }
+
 	public void setUser(World w){
 		this.world = w;
 	}
 
 	public void execute(){
-		
-		for(int i = 0; i < world.getNbAccessPoints(); ++i){
-			
-			AccessPoint a = world.getAccessPoint(i);
-            List<UR> ur = a.getUr();
-            Iterator<UR> it = ur.Iterator();
-            while(it.hasNaxt){
-                 UR ur_current = it.next();
-                 if(){
-                    
-                 }       
-            }
+						
+		AccessPoint a = world.getAccessPoint(0);
+        List<UR> ur = a.getUr();
+        Iterator<UR> it = ur.iterator();
 
-		}
-	
+        while(it.hasNext()){
+             UR ur_current = it.next();
+             User user = ur_current.getUser();
+             if (user != null){
+                this.debitMax += user.getDebitCurrent();
+             }       
+        }
+
+        this.debitMax = this.debitMax / World.MAX_TIME;
 	}
 
 
-	public void finalize() { }
+	public void finalize(){ }
 }
