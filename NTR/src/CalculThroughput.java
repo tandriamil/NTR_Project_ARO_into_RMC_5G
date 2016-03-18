48bd976754693fc2ee804604498e3751f83ca7f7
Quentin GUILLOU <guillou.quentin.29@gmail.com>
2016-03-18 15:17:59 +0100

    Fixing bugs

--- NTR/src/CalculThroughput.java
+++ NTR/src/CalculThroughput.java
@@ -3,6 +3,10 @@ import java.util.List;
 import java.util.Iterator;
 import java.io.File;
 import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
 
 public class CalculThroughput implements Calculation{
 
@@ -41,6 +45,9 @@ public class CalculThroughput implements Calculation{
         }
 	}
 
+	public void finalize(){
+		//Only for fix unimplements bug
+	}
 
 	public void finalize(int nb_Users){
 		this.throughputMax = this.throughputMax / World.MAX_TIME;
@@ -57,7 +64,7 @@ public class CalculThroughput implements Calculation{
 			}
 			//Ouvrir le fichier en lecture et écriture
 			BufferedReader buf = new BufferedReader(new FileReader(nameFile));
-        	BufferedWriter writer = new BufferedWriter(new FileWriter(nameFile;
+        	BufferedWriter writer = new BufferedWriter(new FileWriter(nameFile));
 
 		}catch(IOException e){
 			e.printStackTrace();
