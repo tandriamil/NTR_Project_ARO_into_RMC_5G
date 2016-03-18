e1435887ae4fcc3099a8a8ad65eeea42b242ef28
Ihsine Azzeddine <azd.ihsine@gmail.com>
2016-03-18 10:59:21 +0100

    La classe délai

--- NTR/src/CalculLatency.java
+++ NTR/src/CalculLatency.java
@@ -28,20 +28,24 @@ public class CalculLatency implements Calculation{
 		AccessPoint a = world.getAccessPoint(0);
         List<UR> ur = a.getUr();
         Iterator<UR> it = ur.iterator();
+        int nbr_user = 0;
 
         while(it.hasNext()){
              UR ur_current = it.next();
              User user = ur_current.getUser();
              if (user != null){
+             	nbr_user++;
              	List<Packet> packets = user.getPacket_send();
              	Iterator<Packet> it2 = packets.iterator();
              	while(it2.hasNext()){
-
+             		Packet packet_send = it2.next();
+                	this.latency += (packet_send.getEndSend() - packet_send.getBeginSend());
              	}
-
+             	this.latency = this.latency / packets.size();
              }       
         }
-				
+
+        this.latency = this.latency / nbr_user;			
 	}
 
 	public void finalize(){ }
