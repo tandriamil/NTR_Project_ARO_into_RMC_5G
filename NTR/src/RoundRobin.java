7a5f6099031242cbf611b591b0953561a0e126cf
Quentin Guillou <15009070@e003m06.istic.univ-rennes1.fr>
2016-03-18 11:46:25 +0100

    Improve RR algorithm

--- NTR/src/RoundRobin.java
+++ NTR/src/RoundRobin.java
@@ -9,21 +9,55 @@ public class RoundRobin implements Algorithm {
 		int cpt = 0;
 		Iterator<User> it = users.iterator();
 		Iterator<UR> it_ur = urs.iterator();
-		while(it.hasNext() && it_ur.hasNext()){
-			do {
-				// There is an user 
-				if(!it.hasNext()) {
-					it = users.iterator();
-					cpt++;
-				}
-				user = it.next();
-			} while(user.getCurrentPacket() == null && cpt == 1);
+		// while(it.hasNext() && it_ur.hasNext()){
+		// 	do {
+		// 		// There is an user 
+		// 		if(!it.hasNext()) {
+		// 			it = users.iterator();
+		// 			cpt++;
+		// 		}
+		// 		user = it.next();
+		// 	} while(user.getCurrentPacket() == null && cpt == 1);
 
-			if(user.getCurrentPacket() != null) {
-				ur = it_ur.next();
+		// 	if(user.getCurrentPacket() != null) {
+		// 		ur = it_ur.next();
+		// 		ur.affectURToUser(user);
+		// 		user.checkPacket();
+		// 	}
+		// }
+
+		boolean found = false;
+
+		while(it_ur.hasNext()) {
+			cpt = 0;
+			ur = it_ur.next();
+			user = null;
+			
+			found = false;
+
+			while(!found && cpt < 2) {
+				if(it.hasNext()) {
+					user = it.next();
+					if(user.getCurrentPacket() != null){
+						found = true;
+					}
+					else {
+						user = null;
+					}
+				}
+				else {
+					cpt ++;
+					it = users.iterator();
+				}
+			}
+
+			if(user != null) {
 				ur.affectURToUser(user);
 				user.checkPacket();
 			}
+
+			if(!it.hasNext())
+				it = users.iterator();
 		}
 
 		return urs;
