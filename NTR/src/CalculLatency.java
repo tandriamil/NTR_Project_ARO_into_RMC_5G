beb0357f19901fe73cfc37312d88b80bce8ce73c
Ihsine Azzeddine <azd.ihsine@gmail.com>
2016-04-01 12:03:59 +0200

    La classe ClculLatency

--- NTR/src/CalculLatency.java
+++ NTR/src/CalculLatency.java
@@ -1,18 +1,40 @@
-
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
 
 public class CalculLatency implements Calculation{
 
-	private World world; 
-    private int latency;
+	private World world;
+	private Map<AccessPoint, Integer> latency;
 
+   
+	/**
+	 * Constructor for this computation class
+	 *
+	 * @param worl The world on which we will do the computations
+	 */
 	public CalculLatency(World world){
+
+		//Put the world
 		this.world = world;
-        this.latency = 0;
+
+        // Get a list to put all the results
+		this.latency = new HashMap<AccessPoint, Integer>();
+
+		// Add all the access points of this world
+		for (AccessPoint ap : world.getAccessPoints()) {
+			this.latency.put(ap, 0);
+		}
 	}
 
-	public World getWorld(){
+/*	public World getWorld(){
 		return this.world;
 	}
 
@@ -22,58 +44,91 @@ public class CalculLatency implements Calculation{
 
 	public void setUser(World w){
 		this.world = w;
-	}
+	}*/
 
+	/**
+	 * Function launched at every step to compute the datas
+	 */
 	public void execute(){
-		AccessPoint a = world.getAccessPoint(0);
-        List<UR> ur = a.getUr();
-        Iterator<UR> it = ur.iterator();
-        int nbr_user = 0;
 
-        while(it.hasNext()){
-             UR ur_current = it.next();
-             User user = ur_current.getUser();
-             if (user != null){
-             	nbr_user++;
-             	List<Packet> packets = user.getPacket_send();
-             	Iterator<Packet> it2 = packets.iterator();
-             	while(it2.hasNext()){
-             		Packet packet_send = it2.next();
-                	this.latency += (packet_send.getEndSend() - packet_send.getBeginSend());
-             	}
-             	this.latency = this.latency / packets.size();
-             }       
-        }
+        User user;
 
-        this.latency = this.latency / nbr_user;			
+     	for (AccessPoint ap : this.latency.keySet()){
+     		
+     		int nbUser = 0;
+     		int latency_current = 0; 
+
+     		for(UR ur_current : ap.getUr()){
+
+     			// Get the user put on each UR
+				user = ur_current.getUser();
+				
+				// If there is one
+				if (user != null){
+					nbUser++;
+             		List<Packet> packets = user.getPacket_send();
+             		Iterator<Packet> it2 = packets.iterator();
+             		while(it2.hasNext()){
+
+             			Packet packet_send = it2.next();
+             			latency_current += (packet_send.getEndSend() - packet_send.getBeginSend());
+               		}
+
+             		latency_current = latency_current / packets.size();
+					
+				}
+
+				latency_current = latency_current / nbUser;
+				this.latency.put(ap, this.latency.get(ap) + latency_current);		
+        	}
+	
+     	}
+		
 	}
 
-	public void finalize(int nbr_user){ 
+	public void finalize(int nbUsers){ 
 	
-	/*	String nameFile = "latency.csv"; 
+		// Parameters used here
+		int finalResult, accessPointId = 1;
 		File file = null;
-        BufferedWriter writer = null;
-		
-		try{
+		BufferedWriter writer = null;
+		String nameFile;
 
-			file = new File(nameFile);
+		// For each access point
+		for (AccessPoint ap : this.latency.keySet()) {
 
-			if(file.createNewFile()){
+			// Get the final value
+			finalResult = this.latency.get(ap) / World.MAX_TIME;
 
-				System.out.println("The file was created");
+			// The file writing
+			nameFile = world.getNbAccessPoints() + "_cell_" + world.getResAllocAlg().getName() + "_latency_of_cell_number_" + accessPointId + ".csv"; 
 
-			}else{
+			// Care, exceptions can occur here
+			try {
 
-				System.out.println("Error Can not create this file");
+				// Get a new pointer on the file
+				file = new File(nameFile);
 
+				// Try to create it if it doesn't exist
+				file.createNewFile();
+
+				// Write by appending the results
+				writer = new BufferedWriter(new FileWriter(nameFile, true));
+				writer.write(nbUsers + ";" + finalResult + "\n");
+
+			// All the exception handling
+			} catch(IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					writer.close();
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
 			}
 
-        	writer = new BufferedWriter(new FileWriter(nameFile));
-        	writer.write(this.latency"\n"); 
-        	writer.close(); 
-
-		}catch(IOException e){
-			e.printStackTrace();
-		}*/
+			// Increment the access point id
+			accessPointId++;
+		}
 	}
 }
